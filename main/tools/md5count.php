e05ece79b8519cb8e42bed741c40ba14be4baf9f
WMProject1217 <72447462+WMProject1217@users.noreply.github.com>
2020-10-08 21:05:12 +0800

    Fix error

--- main/tools/md5count.php
+++
@@ -1,6 +0,0 @@
-<?php //By WMProject1217
-echo "<form action='postmd5count.php' method='POST'>";
-echo "<input type='text' name='md5' value=''>";
-echo "<input type='submit' value='提交'>";
-echo "</form>";
-?>
---
+++ main/tools/sha1count.php
@@ -0,0 +1,6 @@
+<?php //By WMProject1217
+echo "<form action='postsha1count.php' method='POST'>";
+echo "<input type='text' name='sha1' value=''>";
+echo "<input type='submit' value='提交'>";
+echo "</form>";
+?>
