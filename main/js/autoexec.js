53d4bc8d98af191f3aa5dcb633718d90b0e8a658
WMProject1217 <72447462+WMProject1217@users.noreply.github.com>
2020-10-24 13:34:07 +0800

    Trying fix error

--- main/js/autoexec.js
+++ main/js/autoexec.js
@@ -1,4 +1,4 @@
-﻿console.log(`Wemakeli Version 0.7.10 Build 231
+﻿console.log(`Wemakeli Version 0.7.11 Build 275
 Build on PHP7.3.4nts
 Made by WMProject1217
 你们可能不知道只用20万赢到578万是什么概念。
--- postlogin.php
+++ postlogin.php
@@ -11,23 +11,24 @@ while(!feof($userdb)) {
     if($userline[1]=="$username"){
         $passwordck=$userline[2];
         $passwordsha1=sha1($password);
-        if("$passwordsha1"=="$passwordck"){
+        if($passwordsha1 <> $passwordck){
+            echo "密码错误，请重新登录<br>";
+            echo "$passwordck<br>";
+            echo $passwordsha1 . "<br>";
+            echo "如果浏览器未响应，请<a href='./accoutlogin.php'>单击此处</a>";
+            //echo "<meta http-equiv='refresh' content=3;url='./accoutlogin.php'>";
+            goto expsk;
+        }else{
             $useruid=$userline[0];
-            $usernamed=fopen("./user/$useruid/usersettings.wmst");
+            $usernamed=fopen("./user/$useruid/usersettings.wmst", "r");
             $userturename=fgets($usernamed);
             fclose($usernamed);
             setcookie("username",$userturename,time()+16777215,'/');
             setcookie("useraccout",$userline[1],time()+16777215,'/');
             setcookie("useruid",$useruid,time()+16777215,'/');
             setcookie("userpassword",$userline[2],time()+16777215,'/');
-            echo "<meta http-equiv='refresh' content=1;url='./'>";
-        }else{
-            echo "密码错误，请重新登录<br>";
-            echo "$passwordck<br>";
-            echo sha1($password) . "<br>";
-            echo "如果浏览器未响应，请<a href='./accoutlogin.php'>单击此处</a>";
-            //echo "<meta http-equiv='refresh' content=3;url='./accoutlogin.php'>";
-            goto expsk;
+            //echo "<meta http-equiv='refresh' content=1;url='./'>";
+            goto edxkx;
         }
     }else{
         goto contrack;
@@ -36,8 +37,14 @@ while(!feof($userdb)) {
 }
 echo "用户不存在，请重新登录<br>";
 echo "如果浏览器未响应，请<a href='./accoutlogin.php'>单击此处</a>";
-echo "<meta http-equiv='refresh' content=3;url='./accoutlogin.php'>";
+//echo "<meta http-equiv='refresh' content=3;url='./accoutlogin.php'>";
 expsk:
 fclose($userdb);
+goto ends;
 }
+edxkx:
+echo "登录成功<br>";
+echo "如果浏览器未响应，请<a href='./index.php'>单击此处</a>";
+//echo "<meta http-equiv='refresh' content=3;url='./index.php'>";
+ends:
 ?>
